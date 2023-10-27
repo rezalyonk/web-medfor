3f46c075772216c7b1ad361663df0b84471a3d6a
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-10-28 00:40:35 +0700

    mengkonfigurasi host gambar

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['mediaformasi.com'], // Ganti dengan nama domain yang sesuai
+  },
 }
 
 module.exports = nextConfig
