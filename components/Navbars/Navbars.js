12a0e9d3c8f5ac62339b032ef0f4646c6344f641
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-11-07 00:21:37 +0700

    delate head

--- components/Navbars/Navbars.js
+++ components/Navbars/Navbars.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Script from "next/script";
 import Image from "next/image";
 import styles from '@/components/Navbars/Navbars.module.css'
@@ -9,17 +8,6 @@ import iconeSearch from '@/assets/images/icon-search.png';
 export default function Navbars() {
   return (
     <div>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Media Formasi</title>
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-          crossOrigin="anonymous"
-        />
-      </Head>
       <div>
         <nav className="navbar bg-body-tertiary">
           <div className="container-fluid">
