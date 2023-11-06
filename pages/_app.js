051abdaeaed215d40178fc87a823ade710aeda5d
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-11-07 00:39:25 +0700

    changes import and load bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import "@/styles/globals.css";
+// import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// import "bootstrap/dist/js/bootstrap.min.js";
+// import "@popperjs/core/dist/umd/popper.min.js";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // Load Bootstrap JavaScript and Popper.js on the client-side
+    import("bootstrap/dist/js/bootstrap.bundle.min.js");
+    import("bootstrap/dist/js/bootstrap.min.js");
+    import("bootstrap/dist/js/bootstrap.js");
+    import("@popperjs/core/dist/umd/popper.min.js");
+  }, []);
+
   return <Component {...pageProps} />;
 }
