1ad054feeeaa90ad5fd7ee0cfd9e8697fd645056
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-11-07 23:06:58 +0700

    add new Artikel Terbaru

---
+++ components/ArtikelTerbaru/indexArtikelTerbaru.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from './styleArtikelTerbaru.module.css'
+import iconArticlenews from "@/assets/images/icon-artikel-news.png"
+import CardArtikelTerbaru from './CardArtikelTerbaru'
+
+export default function ArtikelTerbaru() {
+    return (
+        <div className={`${styles.containerDisplay} container`}>
+            <h1 className={`${styles.iniH1} d-inline-block`}>
+                <Image src={iconArticlenews} width={100} height={100} className={`${styles.imageiconArticlenews}`} />
+                Artikel Terbaru
+            </h1>
+            <h1 className={`${styles.iniH1copy} d-inline-block`}>
+                Tampilkan Semua
+            </h1>
+            <CardArtikelTerbaru />
+        </div>
+    )
+}
