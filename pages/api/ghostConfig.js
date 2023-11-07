9e5a55cefffe5bb6b489123d3c0bf9bb80de2294
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-11-07 23:09:47 +0700

    changes and news config ghost

--- pages/api/ghostConfig.js
+++ pages/api/ghostConfig.js
@@ -16,3 +16,42 @@ export async function getPosts() {
         })
 };
 
+export async function getTags() {
+    return await api.tags
+        .browse({
+            include: ['tags', 'authors'],
+            limit: 7
+        }).catch(err => {
+            throw new Error(err);
+        })
+};
+
+export async function fetchGhostDefault() {
+    try {
+        const response = await fetch('https://admin.mediaformasi.com/ghost/api/content/posts/?key=a6f6c02bf2d968577e72e52a39');
+        if (!response.ok) {
+            throw new Error('Request gagal loh, coba cek lagi');
+        }
+        const data = await response.json();
+        // console.log('Data dari API:', data); // Menampilkan data dari API
+        return data;
+    } catch (error) {
+        // console.error('Kesalahan:', error); // Menampilkan kesalahan jika ada
+        setError(error);
+    }
+}
+
+export async function fetchDataAllPost() {
+    try {
+        const response = await fetch('https://admin.mediaformasi.com/ghost/api/content/posts/?key=a6f6c02bf2d968577e72e52a39&include=tags,authors');
+        if (!response.ok) {
+            throw new Error('Request gagal loh, coba cek lagi');
+        }
+        const data = await response.json();
+        // console.log('Data dari API:', data); // Menampilkan data dari API
+        return data;
+    } catch (error) {
+        console.error('Kesalahan:', error); // Menampilkan kesalahan jika ada
+        setError(error);
+    }
+}
