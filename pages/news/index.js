eb059188eaf649ae9c12dfd66953ac1787895733
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2023-11-10 00:33:03 +0700

    add rotue url pages all news

---
+++ pages/news/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HalamanAllNews from '@/components/AllNews/indexPage.js'
+
+export default function index() {
+    return (
+        <>
+            <HalamanAllNews />
+            <div>
+                <h1>ini harusnya tampilan halaman all news</h1>
+            </div>
+        </>
+    )
+}
