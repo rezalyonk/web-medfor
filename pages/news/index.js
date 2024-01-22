4130371b260333ca8cf78a1e6523586d70be42b3
Reza Lyon Kurniawan <rezalyonk01@gmail.com>
2024-01-22 23:35:47 +0700

    changes and add

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import styles from './index.module.css'
 import HalamanAllNews from '@/components/AllNews/indexPage.js'
 
 export default function index() {
     return (
         <>
-            <HalamanAllNews />
+            {/* <HalamanAllNews /> */}
             <div>
                 <h1>ini harusnya tampilan halaman all news</h1>
             </div>
